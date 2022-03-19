ed3f27f908c19205715e9efa83a9b12513090c97
Eder Lima <devilsouls666@gmail.com>
2022-03-19 10:51:18 -0300

    fix: update main.go file

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"log"
+
+	"github.com/asynched/blockchain/web"
 )
 
 func main() {
-	log.Println("Hello, world!")
+	server := web.MakeServer()
+
+	log.SetFlags(log.Ltime | log.Ldate)
+	log.Println("[INFO] Starting server on port :8081")
+	log.Fatal(server.ListenAndServe())
 }
